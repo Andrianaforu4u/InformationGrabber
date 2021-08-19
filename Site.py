9fd1fecbe0968c9ac911960991acc458731b052a
Andrianaforu4u <87538141+Andrianaforu4u@users.noreply.github.com>
2021-08-19 15:36:36 -0400

    Update Site.py

--- Site.py
+++ Site.py
@@ -4,7 +4,7 @@ def Site():
   
   Type = input("(What type of site do you want) BasicSite = A CoolSite = b (soon) NoDecSite = c (soon): ")
   Name = input("What Is the name for it: ")
-  if type == A:
+  if type == "A":
     f = open(Name+".html", "a")
     f1 = open("Html/BasicSite.html", "r")
     f.write(f1.read())
