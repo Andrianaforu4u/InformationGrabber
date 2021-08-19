7056f7845b73e215d61fe958b7e66d02931e58f7
Andrianaforu4u <87538141+Andrianaforu4u@users.noreply.github.com>
2021-08-19 15:25:12 -0400

    Create Site.py

---
+++ Site.py
@@ -0,0 +1,10 @@
+import time
+
+def Site():
+  
+  Type = input("(What type of site do you want) BasicSite = A CoolSite = b (soon) NoDecSite = c (soon): ")
+  Name = input("What Is the name for it: ")
+  if type == A:
+    f = open(Name+".html", "a")
+    f1 = open("Html/BasicSite.html", "r")
+    f.write(f1.read())
